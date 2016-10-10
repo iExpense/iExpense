931648c421d00f4ec0903c5aa7ad168fdb525129
Narottam <dattatrey93@gmail.com>
2016-10-10 18:21:45 +0530

    expanse editor autosuggestion added

--- app/src/main/java/io/github/iexpense/ExpenseEditorActivity.java
+++ app/src/main/java/io/github/iexpense/ExpenseEditorActivity.java
@@ -2,12 +2,22 @@ package io.github.iexpense;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class ExpenseEditorActivity extends Activity {
+
+    String[] tagSuggestion = {
+            "you paid &#8377 1000 to Aman", "you borrow $ 30 from Aman"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.editor_main);
+        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.autocomplete);
+        actv.setAdapter(new ArrayAdapter<String>(this,R.layout.editor_main,tagSuggestion));
+
     }
 }
