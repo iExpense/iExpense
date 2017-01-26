b51dd620e2653d09f77f66724498193cd73c18dd
Narottam Dattatrey <dattatrey93@gmail.com>
2017-01-26 13:55:10 +0530

    add button added

--- app/src/main/java/io/github/iexpense/ExpenseEditorActivity.java
+++ app/src/main/java/io/github/iexpense/ExpenseEditorActivity.java
@@ -2,8 +2,13 @@ package io.github.iexpense;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ExpenseEditorActivity extends Activity {
 
@@ -14,10 +19,19 @@ public class ExpenseEditorActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.editor_main);
-        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.autocomplete);
-        actv.setAdapter(new ArrayAdapter<String>(this,R.layout.editor_main,tagSuggestion));
+        EditText num = (EditText) findViewById(R.id.editor_textInput);
+//        actv.setAdapter(new ArrayAdapter<String>(this,R.layout.editor_main,tagSuggestion));
+//        Log.d("add", "runs");
+//        final int val = Integer.parseInt(num.getText().toString());
+        Button button = (Button) findViewById(R.id.addExpense);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                int expense = val;
+//
+//                // Perform action on click
+//            }
+//        });
 
     }
 }
